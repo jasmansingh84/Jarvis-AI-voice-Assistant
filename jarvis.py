07c833c5b5233e5531d36badff0c8ae0436285a0
Jasman <54865943+jasmansingh84@users.noreply.github.com>
2020-03-30 01:42:07 -0400

    Google feature
    google.com can be opened easily with the help of a web browser import function

--- jarvis.py
+++ jarvis.py
@@ -69,3 +69,8 @@ if __name__ == "__main__":
         
         elif 'open youtube' in query:
             webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+  
